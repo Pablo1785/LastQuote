2628e4a4e7db323c092c7068d4024000358126ba
Pablo1785 <pablo1785@wp.pl>
2021-12-22 15:49:07 +0100

    Data source status + datasurce repo needs a rewrite - junction repository should depend on component repository not the other way around, otherwise it cannot properly handle missing junctions

--- lib/infrastructure/data_sources/data_source_status_repository.dart
+++ lib/infrastructure/data_sources/data_source_status_repository.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dartz/dartz.dart';
+import 'package:ddd/domain/core/value_objects.dart';
 import 'package:ddd/domain/data_sources/data_source_failure.dart';
 import 'package:ddd/domain/data_sources/data_source_status.dart';
 import 'package:ddd/domain/data_sources/i_data_source_status_repository.dart';
@@ -24,15 +25,15 @@ class DataSourceStatusRepository implements IDataSourceStatusRepository {
       getForCurrentUser() async {
     try {
       final userDocRef = await getIt<FirestoreHelper>().userDocument();
+
       final userDataSourceStatuses = await _firestore
           .collection('user_data_source_statuses')
           .where('user_id', isEqualTo: userDocRef.id)
           .get();
-      return right(
-        userDataSourceStatuses.docs
-            .where((doc) => doc.exists)
-            .map((doc) => DataSourceStatusDto.fromFirestore(doc).toDomain())
-            .toImmutableList(),
+      return _parseJunctionsAndHandleMissing(
+        snapshot: userDataSourceStatuses,
+        dataSourceIds: dataSourceIds,
+        userId: userDocRef.id,
       );
     } on PlatformException catch (exception, stacktrace) {
       if (exception.message!.contains('permission')) {
@@ -42,6 +43,112 @@ class DataSourceStatusRepository implements IDataSourceStatusRepository {
         print(stacktrace.toString());
         return left(const DataSourceFailure.unexpected());
       }
+    }
+  }
+
+  Future<Either<DataSourceFailure, KtList<DataSourceStatus>>>
+      _parseJunctionsAndHandleMissing({
+    required QuerySnapshot<Map<String, dynamic>> snapshot,
+    required List<String> dataSourceIds,
+    required String userId,
+  }) async {
+    List<DataSourceStatus> dataSourceStatuses = snapshot.docs
+        .where(
+          (doc) => doc.exists,
+        )
+        .map(
+          (doc) => DataSourceStatusDto.fromFirestore(doc).toDomain(),
+        )
+        .toList();
+
+    // get loaded dataSources that do not have a junction with current user
+    final newDataSourceStatuses = _createMissingJunctionEntities(
+      dataSourceIds: dataSourceIds,
+      userId: userId,
+      dataSourceIdsWithExistingJunctions: dataSourceStatuses
+          .map(
+            (status) => status.dataSourceId.getOrCrash(),
+          )
+          .toList(),
+    );
+
+    // create missing dataSources
+    final failuresOrSuccesses = await Future.wait(
+      newDataSourceStatuses.map(
+        (e) => create(e),
+      ),
+    );
+
+    // check if all creates successful
+    final someFailureOrUnit = failuresOrSuccesses.firstWhere(
+      (element) => element.isLeft(),
+      orElse: () => right<DataSourceFailure, Unit>(unit),
+    );
+
+    // if any create unsuccessful, pass the creation failure as stream output
+    return someFailureOrUnit.fold(
+      (failure) => left<DataSourceFailure, KtList<DataSourceStatus>>(failure),
+      (_) => _addEntitiesToResultAndReturnSuccessCase(
+        newDataSourceStatuses: newDataSourceStatuses,
+        existingDataSourceStatuses: dataSourceStatuses,
+      ),
+    );
+  }
+
+  Either<DataSourceFailure, KtList<DataSourceStatus>>
+      _addEntitiesToResultAndReturnSuccessCase({
+    required List<DataSourceStatus> newDataSourceStatuses,
+    required List<DataSourceStatus> existingDataSourceStatuses,
+  }) {
+    existingDataSourceStatuses.addAll(newDataSourceStatuses);
+    return right<DataSourceFailure, KtList<DataSourceStatus>>(
+      existingDataSourceStatuses.toImmutableList(),
+    );
+  }
+
+  List<DataSourceStatus> _createMissingJunctionEntities({
+    required List<String> dataSourceIds,
+    required String userId,
+    required List<String> dataSourceIdsWithExistingJunctions,
+  }) {
+    return dataSourceIds
+        .where(
+          (dataSourceId) =>
+              !dataSourceIdsWithExistingJunctions.contains(dataSourceId),
+        )
+        .map(
+          (dataSourceId) => DataSourceStatus.empty().copyWith(
+            dataSourceId: UniqueId.fromUniqueString(
+              dataSourceId,
+            ),
+            userId: UniqueId.fromUniqueString(
+              userId,
+            ),
+            id: JunctionUniqueId(
+              [
+                dataSourceId,
+                userId,
+              ],
+            ),
+          ),
+        )
+        .toList();
+  }
+
+  Either<DataSourceFailure, T> _handleException<T>(
+    Object exception,
+    StackTrace stackTrace,
+  ) {
+    if (exception is FirebaseException &&
+        exception.message!.contains('permission')) {
+      return left(const DataSourceFailure.insufficientPermissions());
+    } else if (exception is FirebaseException &&
+        exception.message!.contains('found')) {
+      return left(const DataSourceFailure.documentNotFound());
+    } else {
+      print(exception.toString());
+      print(stackTrace.toString());
+      return left(const DataSourceFailure.unexpected());
     }
   }
 
@@ -82,25 +189,8 @@ class DataSourceStatusRepository implements IDataSourceStatusRepository {
             dataSourceStatusDtoJson,
           );
       return right(unit);
-    } on PlatformException catch (e, stacktrace) {
+    } on Exception catch (e, stacktrace) {
       return _handleException<Unit>(e, stacktrace);
-    }
-  }
-
-  Either<DataSourceFailure, T> _handleException<T>(
-    Object exception,
-    StackTrace stackTrace,
-  ) {
-    if (exception is FirebaseException &&
-        exception.message!.contains('permission')) {
-      return left(const DataSourceFailure.insufficientPermissions());
-    } else if (exception is FirebaseException &&
-        exception.message!.contains('found')) {
-      return left(const DataSourceFailure.documentNotFound());
-    } else {
-      print(exception.toString());
-      print(stackTrace.toString());
-      return left(const DataSourceFailure.unexpected());
     }
   }
 }
